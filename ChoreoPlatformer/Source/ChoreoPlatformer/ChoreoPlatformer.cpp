f0adc29c41cf44c0a306294379fbe303bdeae0d6
Juliana Montes <julymontes495@gmail.com>
2023-01-30 07:35:32 +0100

    Created project

---
+++ ChoreoPlatformer/Source/ChoreoPlatformer/ChoreoPlatformer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ChoreoPlatformer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ChoreoPlatformer, "ChoreoPlatformer" );
---
+++ ChoreoPlatformer/Source/ChoreoPlatformer/ChoreoPlatformerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChoreoPlatformerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHOREOPLATFORMER_API AChoreoPlatformerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
