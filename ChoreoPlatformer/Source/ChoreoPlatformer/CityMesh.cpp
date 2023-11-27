eab64a46406b7503df476c4ca2c9f4c5641e5d22
Juliana Montes <julymontes495@gmail.com>
2023-11-27 08:40:00 +0100

    Implemented automatic tiling for city meshes

--- ChoreoPlatformer/Source/ChoreoPlatformer/CityMesh.cpp
+++ ChoreoPlatformer/Source/ChoreoPlatformer/CityMesh.cpp
@@ -6,17 +6,30 @@ ACityMesh::ACityMesh()
 	ObjectMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Prop Mesh"));
 	ObjectMesh->SetCollisionResponseToAllChannels(ECR_Ignore);
 	RootComponent = ObjectMesh;
+	TilingMeshes.Add(ObjectMesh);
 }
 
 void ACityMesh::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
 {
 	Super::PostEditChangeProperty(PropertyChangedEvent);
 
-	if (SelectedMesh)
+	if (!SelectedMesh)
 	{
-		ObjectMesh->SetStaticMesh(SelectedMesh);
-		SetActorLabel(SelectedMesh->GetFName().ToString());
+		return;
 	}
+
+	SetActorLabel(SelectedMesh->GetFName().ToString());
+	PaintMaterial();
+	CheckTiling();
+	
+	for (auto Tiled : TilingMeshes)
+	{
+		Tiled->SetStaticMesh(SelectedMesh);
+	}
+}
+
+void ACityMesh::PaintMaterial()
+{
 	if (!ObjectMat)
 	{
 		ObjectMat = ObjectMesh->CreateDynamicMaterialInstance(0, ObjectMesh->GetMaterial(0));
@@ -28,5 +41,51 @@ void ACityMesh::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEve
 	ObjectMat->SetScalarParameterValue(FName("OcclusionDistance"), OcclusionDistance);
 	ObjectMat->SetScalarParameterValue(FName("OcclusionVolume"), OcclusionVolume);
 	ObjectMat->SetScalarParameterValue(FName("Metallic"), bMetallic);
+}
 
+void ACityMesh::CheckTiling()
+{
+	if (Width < 1)
+	{
+		Width = 1;
+	}
+	if (Height < 1)
+	{
+		Height = 1;
+	}
+
+	while (Width*Height > TilingMeshes.Num())
+	{
+		auto Tiled = NewObject<UStaticMeshComponent>(this);
+		Tiled->RegisterComponent();
+		Tiled->SetCollisionResponseToAllChannels(ECR_Ignore);
+		Tiled->SetMaterial(0, ObjectMat);
+		Tiled->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+		TilingMeshes.Add(Tiled);
+	}
+
+	while (Width * Height < TilingMeshes.Num())
+	{
+		auto Tiled = TilingMeshes.Last();
+		TilingMeshes.Remove(Tiled);
+		Tiled->DestroyComponent();
+	}
+
+	float column = 0;
+	float row = 0;
+
+	for (auto Tiled : TilingMeshes)
+	{
+		if (Tiled != ObjectMesh)
+		{
+			Tiled->SetRelativeLocation(FVector(column * XDistance, 0, row * YDistance));
+		}
+		column++;
+		if (column >= Width)
+		{
+			column = 0;
+			row++;
+		}
+	}
+	
 }
--- ChoreoPlatformer/Source/ChoreoPlatformer/CityMesh.h
+++ ChoreoPlatformer/Source/ChoreoPlatformer/CityMesh.h
@@ -17,25 +17,38 @@ public:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	class UStaticMeshComponent* ObjectMesh;
 
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	UStaticMesh* SelectedMesh;
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	FColor ColorSlot1;
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	FColor ColorSlot2;
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	FColor ColorSlot3;
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	bool bOccluded;
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	bool bMetallic;
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	float OcclusionDistance = 400;
-	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Mesh Material")
 	float OcclusionVolume = 700;
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Tiling")
+	int Width = 1;
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Tiling")
+	float XDistance = 100;
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Tiling")
+	int Height = 1;
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Tiling")
+	float YDistance = 200;
 
 protected:
 	UPROPERTY()
 	class UMaterialInstanceDynamic* ObjectMat;
+	UPROPERTY()
+	TArray<UStaticMeshComponent*> TilingMeshes;
 	void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+
+	void PaintMaterial();
+	void CheckTiling();
 };
